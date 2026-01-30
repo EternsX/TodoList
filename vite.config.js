5ff4b15d09da830852c2abd0dc30edbfa1d87a44
Vasili <btriantafyllids45@gmail.com>
2026-01-30 18:24:14 +0100

    changed config settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,   // use Render's assigned port
+    host: '0.0.0.0',                  // bind to all interfaces
+    allowedHosts: ['todolist-obmx.onrender.com'], // whitelist Render host
+  },
 })
